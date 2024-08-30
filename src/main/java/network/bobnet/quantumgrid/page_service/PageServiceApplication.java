cfaea5056690799acb7fdbced278b46c2a973539
Bobesz <bob@bobnet.network>
2024-08-30 22:32:04 +0200

    QUAN-40 Set up project structure using Spring Boot

---
+++ src/main/java/network/bobnet/quantumgrid/page_service/PageServiceApplication.java
@@ -0,0 +1,16 @@
+package network.bobnet.quantumgrid.page_service;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Generated
+@SpringBootApplication
+public class PageServiceApplication {
+
+	@Generated
+	public static void main(String[] args) {
+		SpringApplication.run(PageServiceApplication.class, args);
+	}
+
+}
